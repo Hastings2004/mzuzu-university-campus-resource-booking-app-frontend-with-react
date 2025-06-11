10ac8ebf51118d8942f75b515b66d2232f2ee693
Mr Wise <157286103+Hastings2004@users.noreply.github.com>
2025-06-11 14:07:58 +0200

    routes

--- src/Bookings/viewBooking.jsx
+++ src/Bookings/viewBooking.jsx
@@ -80,7 +80,7 @@ export default function ViewBooking() {
 
             if (res.ok) {
                 alert(data.message || "Booking deleted successfully!");
-                navigate("/bookings"); // Redirect to a suitable page after deletion
+                navigate("/booking"); // Redirect to a suitable page after deletion
             } else {
                 alert(data.message || "Failed to delete booking.");
                 console.error("Failed to delete booking:", data);
